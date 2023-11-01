45eed03efd64fec2a84bc5cc609f4c73aa2e1228
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2023-11-01 17:47:40 -0300

    updated Cros

--- listacheck/src/main/java/com/nocountryc14/listacheck/ListacheckApplication.java
+++ listacheck/src/main/java/com/nocountryc14/listacheck/ListacheckApplication.java
@@ -6,8 +6,10 @@ package com.nocountryc14.listacheck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class ListacheckApplication {
 
 
--- listacheck/src/main/java/com/nocountryc14/listacheck/controller/ShopListController.java
+++ listacheck/src/main/java/com/nocountryc14/listacheck/controller/ShopListController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/shoplist")
 public class ShopListController {
