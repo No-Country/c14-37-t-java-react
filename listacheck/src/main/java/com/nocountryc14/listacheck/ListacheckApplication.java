2576d9ab03ba5720e58b9c6a5fe51be7a9782c37
MartinMSalas <ingmartinmsalas@gmail.com>
2023-11-02 09:35:31 -0300

    Add cors in main app

--- listacheck/src/main/java/com/nocountryc14/listacheck/ListacheckApplication.java
+++ listacheck/src/main/java/com/nocountryc14/listacheck/ListacheckApplication.java
@@ -1,15 +1,15 @@
 package com.nocountryc14.listacheck;
 
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Info;
-//import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@CrossOrigin(origins = "*")
 public class ListacheckApplication {
 
 
@@ -18,20 +18,35 @@ public class ListacheckApplication {
         SpringApplication.run(ListacheckApplication.class, args);
     }
 
-//    @Bean
-//    public OpenAPI customOpenAPI(){
-//        return new OpenAPI()
-//                .info(new Info()
-//                        .title("Listacheck API")
-//                        .version("1.0.0")
-//                        .description("API for Listacheck application" +
-//                                "This is a swagger Test")
-//                        .termsOfService("http://swagger.io/terms")
-//                        .license(new License().name("Apache 2.0")
-//                                .url("http://springdoc.org")));
-//
-//    }
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Listacheck API")
+                        .version("1.0.0")
+                        .description("API for Listacheck application" +
+                                "This is a swagger Test")
+                        .termsOfService("http://swagger.io/terms")
+                        .license(new License().name("Apache 2.0")
+                                .url("http://springdoc.org")));
 
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/shoplist/*").allowedOrigins("*");
+                registry.addMapping("/product/*").allowedOrigins("*");
+                registry.addMapping("/category/*").allowedOrigins("*");
+                registry.addMapping("/note/*").allowedOrigins("*");
+                registry.addMapping("/brand/*").allowedOrigins("*");
+                registry.addMapping("/user/*").allowedOrigins("*");
+            }
+        };
+    }
 
 
 }
